12287c13ae413e08bc4ea74b5d253523f15980ec
ervin.musngi <ervinm@d7softwaresolutions.com>
2024-01-16 11:04:57 +0800

    update: class name

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'd7/zkteco-sdk',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'da3faee2c6c1671626c7a68d70dc59fd2d974fff',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'd7/zkteco-sdk' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'da3faee2c6c1671626c7a68d70dc59fd2d974fff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
